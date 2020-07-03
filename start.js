f0e38c4694d25da78b46bb6a82ec0f01e37bc6ec
HeiSir <heisir@vip.163.com>
2020-07-03 18:55:57 +0800

    update for 2.0

--- start.js
+++ start.js
@@ -7,11 +7,14 @@ const _app = new Vue({
         return { 
             version:'',
             m3u8_url: '',
+            m3u8_urls: '',
+            m3u8_url_prefix:'',
             dlg_header_visible: false,
             dlg_newtask_visible: false,
             config_save_dir:'',
             headers:'',
             myKeyIV:'',
+            myLocalKeyIV:'',
             taskName:'',
             taskIsDelTs:true,
             allVideos:[],
@@ -31,6 +34,9 @@ const _app = new Vue({
             
             ipcRenderer.on('get-config-dir-reply',function(event,data){
                 that.config_save_dir = data;
+            });
+            ipcRenderer.on('open-select-m3u8-reply',function(event,data){
+                that.m3u8_url = data;
             });
 
             ipcRenderer.on('task-add-reply',function(event,data){
@@ -84,11 +90,8 @@ const _app = new Vue({
         clickClose:function(e){
             ipcRenderer.send('hide-windows');
         },
-        clickSetting:function(e){
-            this.tabPane = "setting";
-        },
         clickNewTask:function(e){
-            if(this.config_save_dir == '')
+            if(!this.config_save_dir)
             {
                 this.tabPane = "setting";
                 this.$message({title: '提示',type: 'error',message: "请先设置存储路径，再开始下载视频",offset:100,duration:1000});
@@ -118,8 +121,11 @@ const _app = new Vue({
             }
             else
             {
-                this.$message({title: '提示',type: 'error',message: "请输入正确的M3U8 URL",offset:100,duration:1000});
+                this.$message({title: '提示',type: 'error',message: "请输入正确的M3U8-URL或者导入(.m3u8)文件",offset:100,duration:1000});
             }
+        },
+        clickNewLocalTask:function(e){
+
         },
         clickOpenConfigDir:function(e){
             ipcRenderer.send("open-config-dir");
@@ -135,6 +141,24 @@ const _app = new Vue({
         },
         notifyTaskStatus:function(code,message){
             this.$notify({title: '提示',type: (code == 0? 'success':'error'),message: message,showClose: true,duration:3000,position:'bottom-right'});
+        },
+        clickOpenLogDir:function(e){
+            ipcRenderer.send('open-log-dir');
+        },
+        clickSelectM3U8:function(e){
+            ipcRenderer.send('open-select-m3u8');
+        },
+        dropM3U8File:function(e){
+            e.preventDefault();
+
+            if(!e.dataTransfer || 
+                !e.dataTransfer.files || 
+                e.dataTransfer.files.length == 0)
+            {
+                return;
+            }
+            let p = e.dataTransfer.files[0].path;
+            this.m3u8_url = `file:///${p}`;
         }
     },
     mounted:function(){

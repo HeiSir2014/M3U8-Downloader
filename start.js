db919bd480b21387f54efc348b8c993605066cad
HeiSir <heisir@vip.163.com>
2020-07-06 19:10:43 +0800

    update 2.0

--- start.js
+++ start.js
@@ -8,6 +8,8 @@ const _app = new Vue({
             version:'',
             m3u8_url: '',
             m3u8_urls: '',
+            ts_dir:'',
+            ts_urls:[],
             m3u8_url_prefix:'',
             dlg_header_visible: false,
             dlg_newtask_visible: false,
@@ -18,7 +20,13 @@ const _app = new Vue({
             taskName:'',
             taskIsDelTs:true,
             allVideos:[],
-            tabPane:''
+            tabPane:'',
+            tsMergeType:'speed',
+            tsMergeProgress:0,
+            tsMergeStatus:'',
+            tsMergeMp4Path:'',
+            tsMergeMp4Dir:'',
+            tsTaskName:''
         }
     },
     methods:{
@@ -37,6 +45,13 @@ const _app = new Vue({
             });
             ipcRenderer.on('open-select-m3u8-reply',function(event,data){
                 that.m3u8_url = data;
+            });
+            ipcRenderer.on('open-select-ts-dir-reply',function(event,data){
+                that.ts_dir = data;
+            });
+            ipcRenderer.on('open-select-ts-select-reply',function(event,data){
+                that.ts_urls = data;
+                that.ts_dir = `选择了 [${that.ts_urls.length}] 个视频`;
             });
 
             ipcRenderer.on('task-add-reply',function(event,data){
@@ -65,6 +80,15 @@ const _app = new Vue({
                         return;
                     }
                 }
+            });
+            ipcRenderer.on('start-merge-ts-status',function(event,msg){
+
+                if(msg.progress != -1)  that.tsMergeProgress = msg.progress;
+                if(msg.status) that.tsMergeStatus = msg.status;
+                if(msg.code == 1){
+                    that.tsMergeMp4Dir = msg.dir;
+                    that.tsMergeMp4Path = msg.path;
+                } 
             });
             
             ipcRenderer.on('delvideo-reply',function(event,data){
@@ -114,8 +138,9 @@ const _app = new Vue({
                      headers: this.headers,
                      myKeyIV: this.myKeyIV,
                      taskName: this.taskName,
-                     taskIsDelTs:this.taskIsDelTs
-                     });
+                     taskIsDelTs:this.taskIsDelTs,
+                     url_prefix:this.m3u8_url_prefix
+                });
                 this.dlg_newtask_visible = false;
                 this.taskName = '';
             }
@@ -124,8 +149,22 @@ const _app = new Vue({
                 this.$message({title: '提示',type: 'error',message: "请输入正确的M3U8-URL或者导入(.m3u8)文件",offset:100,duration:1000});
             }
         },
-        clickNewLocalTask:function(e){
-
+        clickNewTaskMuti:function(e){
+            if( this.m3u8_urls != '')
+            {
+                ipcRenderer.send('task-add-muti', { m3u8_urls: this.m3u8_urls,
+                     headers: this.headers,
+                     taskIsDelTs:this.taskIsDelTs,
+                     myKeyIV:'',
+                     taskName:''
+                });
+                this.dlg_newtask_visible = false;
+                this.taskName = '';
+            }
+            else
+            {
+                this.$message({title: '提示',type: 'error',message: "请输入正确的M3U8-URL",offset:100,duration:1000});
+            }
         },
         clickOpenConfigDir:function(e){
             ipcRenderer.send("open-config-dir");
@@ -148,6 +187,31 @@ const _app = new Vue({
         clickSelectM3U8:function(e){
             ipcRenderer.send('open-select-m3u8');
         },
+        clickSelectTSDir:function(e){
+            ipcRenderer.send('open-select-ts-dir');
+        },
+        clickStartMergeTS:function(e){
+            ipcRenderer.send('start-merge-ts',{
+                ts_files:this.ts_urls,
+                mergeType:this.tsMergeType,
+                name:this.tsTaskName
+            });
+        },
+        clickClearMergeTS:function(e){
+            this.ts_dir = '';
+            this.ts_urls = [];
+            this.tsTaskName = '';
+            this.tsMergeMp4Dir = '';
+            this.tsMergeMp4Path = '';
+            this.tsMergeProgress = 0;
+            this.tsMergeStatus = '';
+        },
+        clickOpenMergeTSDir:function(e){
+            ipcRenderer.send('opendir',this.tsMergeMp4Dir);
+        },
+        clickPlayMergeMp4:function(e){
+            ipcRenderer.send('playvideo',this.tsMergeMp4Path);
+        },
         dropM3U8File:function(e){
             e.preventDefault();
 
@@ -159,6 +223,34 @@ const _app = new Vue({
             }
             let p = e.dataTransfer.files[0].path;
             this.m3u8_url = `file:///${p}`;
+        },
+        dropTSFiles:function(e){
+            e.preventDefault();
+
+            if(!e.dataTransfer || 
+                !e.dataTransfer.files || 
+                e.dataTransfer.files.length == 0)
+            {
+                return;
+            }
+            let _filePath = [];
+            for (let index = 0; index < e.dataTransfer.files.length; index++) {
+                const f = e.dataTransfer.files[index];
+                if(f.path.endsWith('.ts') || f.path.endsWith('.TS'))
+                { 
+                    _filePath.push(f.path);
+                }
+            }
+            if(_filePath.length)
+            {
+                this.ts_urls = _filePath;
+                this.ts_dir = `选择了 [${_filePath.length}] 个视频`;
+
+            }else if(e.dataTransfer.files.length == 1)
+            {
+                this.ts_dir = e.dataTransfer.files[0].path;
+                ipcRenderer.send('open-select-ts-dir',e.dataTransfer.files[0].path);
+            }
         }
     },
     mounted:function(){
